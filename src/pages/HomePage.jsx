fa7a1a4aab81de86a2253dbe44a9d101f58c55f2
Radmir <radmir_96@mail.ru>
2023-08-10 00:18:06 +0500

    filter on categories

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,41 @@
 import { useEffect, useState } from 'react'
 import Item from '../components/Item'
 import { fetchAllCharacters } from '../config/api'
+import NotFound from './NotFound'
 
 const HomePage = () => {
     const [mainData, setMainData] = useState([])
     const [search, setSearch] = useState('')
+    const [selectedSpecies, setSelectedSpecies] = useState('')
+    const [selectedStatus, setSelectedStatus] = useState('')
+    const [selectedGender, setSelectedGender] = useState('')
 
     const handleKey = (e) => {
         if (e.key === 'Enter') {
             searchCharacter(search)
+            setSearch('')
         }
+    }
+
+    const buttonClick = () => {
+        searchCharacter(search, selectedSpecies, selectedStatus, selectedGender)
+        setSearch('')
+    }
+
+    const handleFilterSpecies = (species) => {
+        setSelectedSpecies(species)
+        console.log(species)
+        searchCharacter(search, species, selectedStatus, selectedGender)
+    }
+    const handleFilterStatus = (status) => {
+        setSelectedStatus(status)
+        console.log(status)
+        searchCharacter(search, selectedSpecies, status, selectedGender)
+    }
+    const handleFilterGender = (gender) => {
+        setSelectedGender(gender)
+        console.log(gender)
+        searchCharacter(search, selectedSpecies, selectedStatus, gender)
     }
 
     useEffect(() => {
@@ -23,8 +49,19 @@ const HomePage = () => {
             })
     }, [])
 
-    const searchCharacter = (strName) => {
-        fetch(`https://rickandmortyapi.com/api/character/?name=${strName}`)
+    const searchCharacter = (strName, typeSpecies, typeStatus, typeGender) => {
+        let apiUrl = 'https://rickandmortyapi.com/api/character'
+
+        if (strName || typeSpecies || typeStatus || typeGender) {
+            apiUrl += '?'
+
+            if (strName) apiUrl += `name=${strName}`
+            if (typeSpecies) apiUrl += `&species=${typeSpecies}`
+            if (typeStatus) apiUrl += `&status=${typeStatus}`
+            if (typeGender) apiUrl += `&gender=${typeGender}`
+        }
+
+        fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => setMainData(data.results))
             .catch((error) => {
@@ -33,55 +70,88 @@ const HomePage = () => {
     }
 
     return (
-        <div className='flex-1 justify-center items-center mx-5'>
+        <div className='flex-1 justify-center items-center mx-5 pb-5'>
             {/* Header */}
             <div className='flex justify-between p-10 items-center'>
                 {/* Filter */}
-                <input
-                    type='search'
-                    placeholder='Enter name...'
-                    onChange={(e) => setSearch(e.target.value)}
-                    onKeyDown={handleKey}
-                />
+                <div>
+                    <input
+                        className='px-2 py-2 rounded-xl'
+                        type='search'
+                        placeholder='Enter name...'
+                        onChange={(e) => setSearch(e.target.value)}
+                        onKeyDown={handleKey}
+                        value={search}
+                    />
+                    <button
+                        className='bg-[#444955] p-2 rounded-xl ml-2 cursor-pointer text-white hover:text-black hover:bg-white'
+                        onClick={buttonClick}
+                    >
+                        Search
+                    </button>
+                </div>
                 <div className='text-white'>
                     Filter by species:
-                    <select className='ml-2 text-black'>
-                        <option>Human</option>
-                        <option>Alien</option>
+                    <select
+                        onChange={(e) => handleFilterSpecies(e.target.value)}
+                        className='ml-2 text-black cursor-pointer'
+                    >
+                        <option value={'#'}>Choose species</option>
+                        <option value={'human'}>Human</option>
+                        <option value={'humanoid'}>Humanoid</option>
+                        <option value={'alien'}>Alien</option>
+                        <option value={'animal'}>Animal</option>
+                        <option value={'robot'}>Robot</option>
+                        <option value={'mytholog'}>Mythological Creature</option>
+                        <option value={'unknown'}>Unknown</option>
                     </select>
                 </div>
                 <div className='text-white'>
                     Filter by status:
-                    <select className='ml-2 text-black'>
-                        <option>Alive</option>
-                        <option>Dead</option>
-                        <option>Unknown</option>
+                    <select
+                        onChange={(e) => handleFilterStatus(e.target.value)}
+                        className='ml-2 text-black cursor-pointer'
+                    >
+                        <option value={'#'}>Choose status</option>
+                        <option value={'alive'}>Alive</option>
+                        <option value={'dead'}>Dead</option>
+                        <option value={'unknown'}>Unknown</option>
                     </select>
                 </div>
                 <div className='text-white'>
                     Filter by gender:
-                    <select className='ml-2 text-black'>
-                        <option>Female</option>
-                        <option>Male</option>
-                        <option>Genderless</option>
-                        <option>Unknown</option>
+                    <select
+                        onChange={(e) => handleFilterGender(e.target.value)}
+                        className='ml-2 text-black cursor-pointer'
+                    >
+                        <option value={'#'}>Choose gender</option>
+                        <option value={'female'}>Female</option>
+                        <option value={'male'}>Male</option>
+                        <option value={'genderless '}>Genderless</option>
+                        <option value={'unknown'}>Unknown</option>
                     </select>
                 </div>
             </div>
 
             {/* Content */}
             <div className='grid grid-cols-3 gap-4'>
-                {mainData.map((item) => (
-                    <Item
-                        key={item.id}
-                        image={item.image}
-                        name={item.name}
-                        status={item.status}
-                        species={item.species}
-                        gender={item.gender}
-                        location={item.location.name}
-                    />
-                ))}
+                {!mainData ? (
+                    <div className='col-span-3 flex justify-center items-center'>
+                        <NotFound />
+                    </div>
+                ) : (
+                    mainData.map((item) => (
+                        <Item
+                            key={item.id}
+                            image={item.image}
+                            name={item.name}
+                            status={item.status}
+                            species={item.species}
+                            gender={item.gender}
+                            location={item.location.name}
+                        />
+                    ))
+                )}
             </div>
         </div>
     )
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+    return <div className='text-4xl text-white'>404 Not Found</div>
+}
+
+export default NotFound
