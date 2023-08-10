d76356788ddd58b5c63d9c95c6d380aa70407d62
exb <radmir_96@mail.ru>
2023-08-10 12:22:48 +0500

    refactor code

---
+++ src/components/Filters.jsx
@@ -0,0 +1,104 @@
+const Filters = ({
+    search,
+    setSearch,
+    selectedSpecies,
+    setSelectedSpecies,
+    selectedStatus,
+    setSelectedStatus,
+    selectedGender,
+    setSelectedGender,
+    searchCharacter,
+}) => {
+    const checkFilters = () => {
+        if (search !== '') {
+            searchCharacter(search, selectedSpecies, selectedStatus, selectedGender)
+        } else {
+            searchCharacter('', selectedSpecies, selectedStatus, selectedGender)
+        }
+    }
+
+    const handleKey = (e) => {
+        if (e.key === 'Enter') {
+            checkFilters()
+        }
+    }
+
+    const buttonClick = () => {
+        searchCharacter(search, selectedSpecies, selectedStatus, selectedGender)
+        checkFilters()
+    }
+
+    const handleFilterSpecies = (species) => {
+        setSelectedSpecies(species)
+        console.log(species)
+        searchCharacter(search, species, selectedStatus, selectedGender)
+    }
+    const handleFilterStatus = (status) => {
+        setSelectedStatus(status)
+        console.log(status)
+        searchCharacter(search, selectedSpecies, status, selectedGender)
+    }
+    const handleFilterGender = (gender) => {
+        setSelectedGender(gender)
+        console.log(gender)
+        searchCharacter(search, selectedSpecies, selectedStatus, gender)
+    }
+
+    return (
+        <div className='flex justify-between p-5 my-5 items-center bg-[#444955] rounded-xl'>
+            <div>
+                <input
+                    className='px-2 py-2 rounded-xl'
+                    type='search'
+                    placeholder='Enter name...'
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKey}
+                    value={search}
+                />
+                <button
+                    className='bg-[#2c3038] p-2 rounded-xl ml-2 cursor-pointer text-white hover:text-black hover:bg-white'
+                    onClick={buttonClick}
+                >
+                    Search
+                </button>
+            </div>
+            <div className='text-white'>
+                Filter by species:
+                <select
+                    onChange={(e) => handleFilterSpecies(e.target.value)}
+                    className='ml-2 text-black cursor-pointer'
+                >
+                    <option value={'#'}>Choose species</option>
+                    <option value={'human'}>Human</option>
+                    <option value={'humanoid'}>Humanoid</option>
+                    <option value={'alien'}>Alien</option>
+                    <option value={'animal'}>Animal</option>
+                    <option value={'robot'}>Robot</option>
+                    <option value={'mytholog'}>Mythological Creature</option>
+                    <option value={'unknown'}>Unknown</option>
+                </select>
+            </div>
+            <div className='text-white'>
+                Filter by status:
+                <select onChange={(e) => handleFilterStatus(e.target.value)} className='ml-2 text-black cursor-pointer'>
+                    <option value={'#'}>Choose status</option>
+                    <option value={'alive'}>Alive</option>
+                    <option value={'dead'}>Dead</option>
+                    <option value={'unknown'}>Unknown</option>
+                </select>
+            </div>
+            <div className='text-white'>
+                Filter by gender:
+                <select onChange={(e) => handleFilterGender(e.target.value)} className='ml-2 text-black cursor-pointer'>
+                    <option value={'#'}>Choose gender</option>
+                    <option value={'female'}>Female</option>
+                    <option value={'male'}>Male</option>
+                    <option value={'genderless '}>Genderless</option>
+                    <option value={'unknown'}>Unknown</option>
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default Filters
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import Item from '../components/Item'
 import { fetchAllCharacters } from '../config/api'
 import NotFound from './NotFound'
+import Filters from '../components/Filters'
 
 const HomePage = () => {
     const [mainData, setMainData] = useState([])
@@ -9,43 +10,6 @@ const HomePage = () => {
     const [selectedSpecies, setSelectedSpecies] = useState('')
     const [selectedStatus, setSelectedStatus] = useState('')
     const [selectedGender, setSelectedGender] = useState('')
-
-    const checkFilters = () => {
-        if (search !== '') {
-            searchCharacter(search, selectedSpecies, selectedStatus, selectedGender)
-        } else {
-            searchCharacter('', selectedSpecies, selectedStatus, selectedGender)
-        }
-    }
-
-    const handleKey = (e) => {
-        if (e.key === 'Enter') {
-            checkFilters()
-            setSearch('')
-        }
-    }
-
-    const buttonClick = () => {
-        searchCharacter(search, selectedSpecies, selectedStatus, selectedGender)
-        checkFilters()
-        setSearch('')
-    }
-
-    const handleFilterSpecies = (species) => {
-        setSelectedSpecies(species)
-        console.log(species)
-        searchCharacter(search, species, selectedStatus, selectedGender)
-    }
-    const handleFilterStatus = (status) => {
-        setSelectedStatus(status)
-        console.log(status)
-        searchCharacter(search, selectedSpecies, status, selectedGender)
-    }
-    const handleFilterGender = (gender) => {
-        setSelectedGender(gender)
-        console.log(gender)
-        searchCharacter(search, selectedSpecies, selectedStatus, gender)
-    }
 
     useEffect(() => {
         fetchAllCharacters()
@@ -82,67 +46,18 @@ const HomePage = () => {
 
     return (
         <div className='flex-1 justify-center items-center mx-5 pb-5'>
-            {/* Header */}
-            <div className='flex justify-between p-10 items-center'>
-                {/* Filter */}
-                <div>
-                    <input
-                        className='px-2 py-2 rounded-xl'
-                        type='search'
-                        placeholder='Enter name...'
-                        onChange={(e) => setSearch(e.target.value)}
-                        onKeyDown={handleKey}
-                        value={search}
-                    />
-                    <button
-                        className='bg-[#444955] p-2 rounded-xl ml-2 cursor-pointer text-white hover:text-black hover:bg-white'
-                        onClick={buttonClick}
-                    >
-                        Search
-                    </button>
-                </div>
-                <div className='text-white'>
-                    Filter by species:
-                    <select
-                        onChange={(e) => handleFilterSpecies(e.target.value)}
-                        className='ml-2 text-black cursor-pointer'
-                    >
-                        <option value={'#'}>Choose species</option>
-                        <option value={'human'}>Human</option>
-                        <option value={'humanoid'}>Humanoid</option>
-                        <option value={'alien'}>Alien</option>
-                        <option value={'animal'}>Animal</option>
-                        <option value={'robot'}>Robot</option>
-                        <option value={'mytholog'}>Mythological Creature</option>
-                        <option value={'unknown'}>Unknown</option>
-                    </select>
-                </div>
-                <div className='text-white'>
-                    Filter by status:
-                    <select
-                        onChange={(e) => handleFilterStatus(e.target.value)}
-                        className='ml-2 text-black cursor-pointer'
-                    >
-                        <option value={'#'}>Choose status</option>
-                        <option value={'alive'}>Alive</option>
-                        <option value={'dead'}>Dead</option>
-                        <option value={'unknown'}>Unknown</option>
-                    </select>
-                </div>
-                <div className='text-white'>
-                    Filter by gender:
-                    <select
-                        onChange={(e) => handleFilterGender(e.target.value)}
-                        className='ml-2 text-black cursor-pointer'
-                    >
-                        <option value={'#'}>Choose gender</option>
-                        <option value={'female'}>Female</option>
-                        <option value={'male'}>Male</option>
-                        <option value={'genderless '}>Genderless</option>
-                        <option value={'unknown'}>Unknown</option>
-                    </select>
-                </div>
-            </div>
+            {/* Filter */}
+            <Filters
+                search={search}
+                setSearch={setSearch}
+                selectedSpecies={selectedSpecies}
+                setSelectedSpecies={setSelectedSpecies}
+                selectedStatus={selectedStatus}
+                setSelectedStatus={setSelectedStatus}
+                selectedGender={selectedGender}
+                setSelectedGender={setSelectedGender}
+                searchCharacter={searchCharacter}
+            />
 
             {/* Content */}
             <div className='grid grid-cols-3 gap-4'>
